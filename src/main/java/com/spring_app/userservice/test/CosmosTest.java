073ba9790f76f80055875fbbce177c40b8969003
debabratakundu <debabrata.kundu@rci.com>
2025-07-29 22:25:26 +0530

    remove secret key

--- src/main/java/com/spring_app/userservice/test/CosmosTest.java
+++ src/main/java/com/spring_app/userservice/test/CosmosTest.java
@@ -11,7 +11,7 @@ public class CosmosTest {
     public static void main(String[] args) {
         CosmosClient client = new CosmosClientBuilder()
                 .endpoint("https://cosmos-bookstore-db.documents.azure.com:443/")
-                .key("p8U96XpO89Dr8bouT70w6KZeA6tdjEJJz88udYxv4kD8FS5RLkkpDGx1UZploHCa0DTr2e3kKXn2ACDbSAPx6A==")
+                .key("xxx")
                 .consistencyLevel(ConsistencyLevel.EVENTUAL)
                 .buildClient();
 
